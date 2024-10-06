c9eb872fa16d5aad439305973fe2d7acf23718ae
Nsikak-mbre <tm260715@gmail.com>
2024-10-06 14:25:52 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const [photoResponse, userResponse] = values;
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
