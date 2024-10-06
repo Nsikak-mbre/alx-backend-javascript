a7ab4d62c37b6da3aae9e1aa9780fc047b05af53
Nsikak-mbre <tm260715@gmail.com>
2024-10-06 13:11:45 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,8 +4,11 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return {
         status: 200,
-        body: 'Success',
+        body: 'success',
       };
     })
-    .catch(() => new Error());
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
