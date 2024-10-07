303660e9b2628d2fc65d7670232931318aefd0c8
Nsikak-mbre <tm260715@gmail.com>
2024-10-07 23:59:29 +0100

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,31 +1,14 @@
 class Building {
   constructor(sqft) {
-    // if (new.target === Building) {
-    //   throw new TypeError('Cannot instantiate abstract class Building directly');
-    // }
-
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     this._sqft = sqft;
   }
 
-  // Getter
   get sqft() {
     return this._sqft;
   }
 
-  // Setters
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = sqft;
-  }
-
-  // Abstract method
   evacuationWarningMessage() {
-    throw new Error(`Class extending Building with ${this.sqft} must override evacuationWarningMessage`);
+    throw new Error(`Class ${this.constructor.name} extending Building must override evacuationWarningMessage`);
   }
 }
 
