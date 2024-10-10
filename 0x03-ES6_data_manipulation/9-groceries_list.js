4493cd79980f7a710f83fab400a06757537be103
Nsikak-mbre <tm260715@gmail.com>
2024-10-10 13:05:57 +0100

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,9 @@
-const groceriesList = () => {
-    return new Map([
-        ['Apples', 10],
-        ['Tomatoes', 10],
-        ['Pasta', 1],
-        ['Rice', 1],
-        ['Banana', 5]
-    ]);
-};
+const groceriesList = () => new Map([
+  ['Apples', 10],
+  ['Tomatoes', 10],
+  ['Pasta', 1],
+  ['Rice', 1],
+  ['Banana', 5],
+]);
 
-export default groceriesList
+export default groceriesList;
