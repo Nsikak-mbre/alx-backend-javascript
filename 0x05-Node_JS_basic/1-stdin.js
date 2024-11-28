5a40462b10468ac206ce8b7b9525f4b3a6517c9e
Nsikak-mbre <tm260715@gmail.com>
2024-11-28 02:04:25 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,16 @@
-// Import the readline module
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create the readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Flag to track if input was processed
-let inputHandled = false;
-
-// Display a message to the user and prompt for input
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  inputHandled = true; // Mark that input has been processed
-  rl.close(); // Explicitly close the readline interface
-});
-
-// Handle the close event to display the closing message
-rl.on('close', () => {
-  if (inputHandled) {
-    console.log('This important software is now closing');
-  }
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
