d5482e5e70770dafe9621be9b647856de9c35bf0
Nsikak-mbre <tm260715@gmail.com>
2024-12-06 21:00:38 +0100

    start

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-// Test Suite for Rounding the First Number
 describe('First Number Rounding', function () {
    it('should round the first number and sum', function () {
       assert.equal(calculateNumber(1.6, 1), 3);
    });
 });
 
-// Test Suite for Rounding the Second Number
 describe('Second Number Rounding', function () {
    it('should round the second number and sum', function () {
       assert.equal(calculateNumber(1, 1.6), 3);
    });
 });
 
-// Test Suite for Rounding Both Numbers
 describe('Both Numbers Rounding', function () {
    it('should round both numbers and sum', function () {
       assert.equal(calculateNumber(1.6, 1.7), 4);
    });
 });
 
-// Additional Test Suite for Basic Sum
 describe('Sum', function () {
    it('should return 2 when 1 + 1', function () {
       assert.equal(calculateNumber(1, 1), 2);
