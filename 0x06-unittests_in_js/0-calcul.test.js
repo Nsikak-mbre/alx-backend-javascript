34bd4b92b88f165437b1d8e4a2d6c9515358bb40
Nsikak-mbre <tm260715@gmail.com>
2024-12-06 20:31:20 +0100

    start

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+describe('Sum', function () {
+   it('should return 2 when 1 + 1', function () {
+      assert.equal(calculateNumber(1, 1), 2);
+   });
+
+   it('should round given numbers to the nearest integer', function () {
+      assert.equal(calculateNumber(1.5, 1.5), 4);
+   });
+   it('sum of whole numbers with floating point numbers should return whole number', function () {
+      assert.equal(calculateNumber(1 , 1.5), 3);
+   });
+});
