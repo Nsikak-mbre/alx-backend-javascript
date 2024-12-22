d22005735b384c53f92b949fe283269e78949a14
Nsikak-mbre <tm260715@gmail.com>
2024-12-22 02:52:22 +0100

    9-api

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('should return correct status code', (done) => {
+    request.get(API_URL, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct response body', (done) => {
+    request.get(API_URL, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should handle non-existent routes', (done) => {
+    request.get(`${API_URL}/nonexistent`, (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
