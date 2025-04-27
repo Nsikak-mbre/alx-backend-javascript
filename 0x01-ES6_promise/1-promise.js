9aece88ac9e27bd720ef13e3d811b19b0bc254e9
Nsikak-mbre <tm260715@gmail.com>
2025-04-27 16:02:28 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,21 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+// export default function getFullResponseFromAPI(success) {
+//   return new Promise((resolve, reject) => {
+//     if (success) {
+//       resolve({
+//         status: 200,
+//         body: 'Success',
+//       });
+//     } else {
+//       reject(new Error('The fake API is not working currently'));
+//     }
+//   });
+// }
+
+
+export async function getFullResponseFromAPI(success) {
+  if (success) {
+    return { status: 200, body: "Success" };
+  } else {
+    throw new Error("The fake API is not working currently");
+  }
 }
